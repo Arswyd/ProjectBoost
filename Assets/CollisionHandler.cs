866094965800b76a21fafa16f248c3a73cab1597
Arswyd <vdani94@gmail.com>
2021-02-17 23:25:45 +0100

    Updated according to new course version

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // PARAMETERS
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip deathExplosion;
+    [SerializeField] AudioClip levelComplete;
+
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem levelCompleteParticles;
+
+    // CACHE
+
+    AudioSource audioSource;
+
+    // STATE
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toggle collision
+            if (collisionDisabled)
+            {
+                Debug.Log("Collision Disabled");
+            }
+            else
+            {
+                Debug.Log("Collision Enabled");
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning || collisionDisabled)
+        {
+            return; // Guard condition -> if not alive don't execute further code
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartFinishSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartFinishSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(levelComplete);
+        levelCompleteParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay); // Invoke calls a method after a certain time (declared in seconds)
+    }
+
+    private void StartDeathSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathExplosion);
+        deathParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // PARAMETERS
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainEngineParticles;
+
+    // CACHE
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // STATE
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            ProcessThrust();
+            ProcessRotation();
+        }
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) // can thrust while rotating
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            StopThrust();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(rcsThrust);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(-rcsThrust);
+        }
+
+        rigidBody.freezeRotation = false; // resumes phisics rotation
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!mainEngineParticles.isPlaying)
+        {
+            mainEngineParticles.Play();
+        }
+    }
+
+    private void StopThrust()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+}
